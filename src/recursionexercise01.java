c983bb21d0629e6cb6f3b0e7497e4373b92d99c6
Jiawei Zhou <zhou.jiaw@northeastern.edu>
2022-10-13 18:52:01 -0600

    recursionexerice

--- src/recursionexercise01.java
+++ src/recursionexercise01.java
@@ -1,10 +1,15 @@
 public class recursionexercise01 {
     public static void main(String[] args){
         tool sum = new tool();
-        int n = 7;
-        int res = sum.fibonacci(n);
-        if (res != -1) {
-            System.out.println("when n =" +n +"correspondding fibonacci number =" + res);
+//        int n = 7;
+//        int res = sum.fibonacci(n);
+//        if (res != -1) {
+//            System.out.println("when n =" +n +"correspondding fibonacci number =" + res);
+//        }
+        int day = 9;
+        int peach = sum.monkeyeat(day);
+        if (peach != -1) {
+            System.out.println(peach);
         }
     }
 }
@@ -18,6 +23,16 @@ class tool {
             }
         } else {
             System.out.println("error input");
+            return -1;
+        }
+    }
+    public int monkeyeat(int day) {
+        if (day ==10){
+            return 1;
+        } else if (day >=1 && day <=9) {
+            return (monkeyeat(day+1)+1) *2;
+        } else {
+            System.out.println("wrong input for day");
             return -1;
         }
     }
